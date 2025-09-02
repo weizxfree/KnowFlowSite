5ac16e30d64d61958aece7ec30606a8f0d161869
weizxfree <weizhangxiang2022@gmail.com>
2025-09-02 19:04:03 +0800

    1. 新域名

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://weizxfree.github.io',
+  url: 'https://www.knowflowchat.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/KnowFlowSite/',
+  // For custom domain deployment, use root path
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
